9e8ae6a328d07a895f2e4575d99c58166504bdcb
Patricio Gonzalez <patote.gonzalez@gmail.com>
2021-05-01 13:10:31 -0400

    basics

---
+++ src/components/Jumbo.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { StyledJumbo } from "./../styles/components"
+export default function Jumbo() {
+  return (
+    <StyledJumbo>
+      <div>
+        <h1>Consigue el mejor swag de platzi!</h1>
+        <small>Piezas elegantes para los mejores estudiantes</small>
+      </div>
+    </StyledJumbo>
+  )
+}
